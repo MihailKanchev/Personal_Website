3cac8625e4bdadc8437524ac55fbf150966a3c20
Mihail Kanchev <colty.gaming@gmail.com>
2021-02-01 17:50:25 +0100

    Revert "Background and ui set up. Blog-post display needs fixing."
    This reverts commit e94528aef83607a1090faa5bff8c90b59ba8daf0.

--- Models/Post.cs
+++ Models/Post.cs
@@ -25,10 +25,18 @@ namespace BlazorCosmicBlog.Models
         public string url { get; set; }
         public string imgix_url { get; set; }
     }
+<<<<<<< HEAD
     public class Author
     {
         public string slug { get; set; }
         public string status { get; set; }
         public string title { get; set; }
+=======
+
+    public class Logo
+    {
+        public string url { get; set; }
+        public string imgix_url { get; set; }
+>>>>>>> parent of e94528a... Background and ui set up. Blog-post display needs fixing.
     }
 }
