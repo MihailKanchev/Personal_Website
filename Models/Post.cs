7bb349db8acdf3047b9bf01e258516f0fddabc13
Mihail Kanchev <colty.gaming@gmail.com>
2021-02-01 17:48:47 +0100

    Revert "Merge branch 'test'"
    This reverts commit bc9e29771cccabd43a298766423396f6ee90a587.

--- Models/Post.cs
+++ Models/Post.cs
@@ -18,7 +18,6 @@ namespace BlazorCosmicBlog.Models
     public class PostMetadata
     {
         public Hero hero { get; set; }
-        public string Type { get; set; }
         //other
     }
     public class Hero
@@ -26,5 +25,10 @@ namespace BlazorCosmicBlog.Models
         public string url { get; set; }
         public string imgix_url { get; set; }
     }
-
+    public class Author
+    {
+        public string slug { get; set; }
+        public string status { get; set; }
+        public string title { get; set; }
+    }
 }
